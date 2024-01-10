f29ea023ffd00a55567fd630cc89209e09979952
JFeintzeig <JFeintzeig@users.noreply.github.com>
2024-01-09 21:58:18 -0500

    1.6 done

--- set1/main.go
+++ set1/main.go
@@ -275,10 +275,161 @@ I go crazy when I hear a cymbal`
   }
 }
 
+func HammingDistance(b1 []byte, b2 []byte) int {
+	xor := make([]byte, len(b1))
+	for i := range b1 {
+		xor[i] = b1[i] ^ b2[i]
+	}
+
+  var output int
+  for _, b := range xor {
+    for j := 0; j < 8; j++ {
+      output += int((b >> j) & 0x01)
+    }
+
+  }
+  return output
+}
+
+// this seems like overkill but i needed to do this to get the right
+// keysize. instead of calculating the difference for the first two
+// keysize-length chunks, i take the cross product of all chunks.
+// this makes the average for all the wrong ones to be tightly
+// distributed around ~3.21 +/0 ~0.005, vs. the correct one is ~2.7
+func CalcDistanceForKeySize(input []byte, keysize int) float64 {
+  chunks := make([][]byte, 0)
+  for i := 0; i < len(input)/keysize; i++ {
+    chunks = append(chunks, input[keysize*i:keysize*(i+1)])
+  }
+
+  var numerator float64
+  var denominator float64
+  for i, b1 := range chunks {
+    for j, b2 := range chunks {
+      if i != j {
+        numerator += float64(HammingDistance(b1, b2))
+        denominator += 1
+      }
+    }
+  }
+  return numerator / (denominator * float64(keysize))
+}
+
+func TransposeByteArrayByKeySize(byteArray []byte, keysize int) [][]byte {
+  // Break `byteArray` into `keysize` pieces, each of length `byteArray/keysize`
+  // Except some pieces could be 1 entry longer, if byteArray is not equally
+  // divisible by keysize
+
+  output := make([][]byte, keysize)
+  for i := 0; i < keysize; i++ {
+    output[i] = make([]byte, 0)
+  }
+
+  iterate := true
+  // stride through `keysize`-length chunks, i is the chunk number
+  // j is the byte number within each chunk, which represents which
+  // output piece it should be put into
+  var i int
+  for iterate {
+    var chunk []byte
+    // if our next stride of length `keysize` goes beyond the end of the array
+    // then just grab whatever's remaining, and this is last iteration of loop
+    if (i+1)*keysize > len(byteArray) {
+      iterate = false
+      chunk = byteArray[i*keysize:]
+    } else {
+      chunk = byteArray[i*keysize:(i+1)*keysize]
+    }
+    for j, b := range chunk {
+      output[j] = append(output[j], b)
+    }
+    i += 1
+  }
+  return output
+}
+
+func Challenge6() {
+  fmt.Printf("Challenge 6:\n")
+  s1 := "this is a test"
+  s2 := "wokka wokka!!!"
+  if HammingDistance([]byte(s1), []byte(s2)) != 37 {
+    panic("hamming distance wokka wokka test fails")
+  }
+
+  file, err := os.ReadFile("6.txt")
+  if err != nil {
+    panic("problem opening file")
+  }
+
+  input, err := base64.StdEncoding.DecodeString(string(file))
+  if err != nil {
+    panic("problem decoding b64")
+  }
+
+  minDist := 10.0
+  distSlice := make([]float64, 0)
+  var actualKeySize int
+  var idxForKey int
+  for keysize := 2; keysize <= 40; keysize++ {
+    dist := CalcDistanceForKeySize(input, keysize)
+    if dist < minDist {
+      minDist = dist
+      actualKeySize = keysize
+      idxForKey = keysize - 2
+    }
+    distSlice = append(distSlice, dist)
+  }
+
+  // remove minimum
+  distSlice = append(distSlice[:idxForKey], distSlice[idxForKey+1:]...)
+
+  var distSum float64
+  for _, v := range distSlice {
+    distSum += v
+  }
+  avgDist := distSum / float64(len(distSlice))
+
+  variance := 0.0
+  for _, v := range distSlice {
+    variance += math.Pow(v - avgDist, 2)
+  }
+
+  fmt.Printf("\nKeysize: %d MinDist: %3.3f vs others have %3.3f +/- %3.3f\n", actualKeySize, minDist, avgDist, math.Sqrt(variance))
+
+  // here i use chunk to signify the transposed result, but inside this
+  // function i use chunk to signify a stride of the original. sorry for
+  // the confused naming
+  transposedChunks := TransposeByteArrayByKeySize(input, actualKeySize)
+  decryptedChunks := make([][]byte, actualKeySize)
+  for i, tc := range transposedChunks {
+    thisDecrypted, _, _ := DecryptSingleByteXOR(tc)
+    decryptedChunks[i] = []byte(thisDecrypted)
+  }
+
+  output := make([]byte, 0)
+  // if some chunks have 1 extra entry, the first chunk will
+  for i := 0; i < len(decryptedChunks[0]); i++ {
+    for j := range decryptedChunks {
+      // so make sure to escape once we've used up all extra entries
+      if i >= len(decryptedChunks[j]) {
+        break
+      }
+      output = append(output, decryptedChunks[j][i])
+    }
+  }
+
+  fmt.Printf("\nSolution:\n%s\n", string(output))
+
+  if len(input) != len(output) {
+    panic("uh oh, challenge 6 input + output have different lengths")
+  }
+}
+
 func main() {
 	Challenge1()
 	Challenge2()
 	Challenge3()
   Challenge4()
   Challenge5()
+  Challenge6()
 }
