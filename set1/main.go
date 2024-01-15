64ce244c989cee8b2415ed2d7188055184f3c18c
JFeintzeig <JFeintzeig@users.noreply.github.com>
2024-01-14 19:18:00 -0500

    go fmt

--- set1/main.go
+++ set1/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "crypto/aes"
+	"crypto/aes"
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"jfeintzeig/cryptopals/lib"
 	"math"
 	"os"
-  "strings"
-  "jfeintzeig/cryptopals/lib"
+	"strings"
 )
 
 var CharFreq map[rune]float64
@@ -149,7 +149,7 @@ func Challenge2() {
 	}
 
 	xor := cryptopals.FixedXOR(b1, b2)
-  xorString := hex.EncodeToString(xor)
+	xorString := hex.EncodeToString(xor)
 	fmt.Printf("Challenge 2: %s\n", xorString)
 	if xorString != "746865206b696420646f6e277420706c6179" {
 		panic("problem with challenge 2")
@@ -158,7 +158,7 @@ func Challenge2() {
 }
 
 func SingleByteXOR(byteArray []byte, key byte) []byte {
-  output := make([]byte, len(byteArray))
+	output := make([]byte, len(byteArray))
 	for i := range byteArray {
 		output[i] = byteArray[i] ^ key
 	}
@@ -202,7 +202,7 @@ func DecryptSingleByteXOR(inputBytes []byte) (string, uint8, float64) {
 			decrypted = outputString
 		}
 	}
-  return decrypted, minKey, minScore
+	return decrypted, minKey, minScore
 }
 
 func Challenge3() {
@@ -212,58 +212,58 @@ func Challenge3() {
 		panic("error decoding hex string")
 	}
 
-  decrypted, minKey, minScore := DecryptSingleByteXOR(inputBytes)
-  fmt.Printf("Challenge 3: key: %02X score: %3.3f decrypted message: %s\n", minKey, minScore, decrypted)
-  if decrypted != "Cooking MC's like a pound of bacon" {
-    panic("problem with challenge 3")
-  }
+	decrypted, minKey, minScore := DecryptSingleByteXOR(inputBytes)
+	fmt.Printf("Challenge 3: key: %02X score: %3.3f decrypted message: %s\n", minKey, minScore, decrypted)
+	if decrypted != "Cooking MC's like a pound of bacon" {
+		panic("problem with challenge 3")
+	}
 }
 
 func Challenge4() {
-  file, err := os.ReadFile("4.txt")
-  if err != nil {
-    panic(err)
-  }
-  lines := strings.Split(strings.Trim(string(file),"\n"), "\n")
+	file, err := os.ReadFile("4.txt")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(strings.Trim(string(file), "\n"), "\n")
 
-  var key uint8
-  var minScore float64 = 10000
-  var decrypted string
-  var input string
-  for _, line := range lines {
-    inputBytes, err := hex.DecodeString(line)
-    if err != nil {
-      panic("error decoding hex string")
-    }
-    thisDecrypted, thisKey, thisScore := DecryptSingleByteXOR(inputBytes)
-    if thisScore < minScore {
-      key = thisKey
-      decrypted = thisDecrypted
-      minScore = thisScore
-      input = line
-    }
-  }
-  fmt.Printf("Challenge 4: input:%s decrypted:%s key:%02X score:%3.3f\n", input, decrypted, key, minScore)
-  if input != "7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f" && key != 0x34 {
-    panic("problem w challenge 4")
-  }
+	var key uint8
+	var minScore float64 = 10000
+	var decrypted string
+	var input string
+	for _, line := range lines {
+		inputBytes, err := hex.DecodeString(line)
+		if err != nil {
+			panic("error decoding hex string")
+		}
+		thisDecrypted, thisKey, thisScore := DecryptSingleByteXOR(inputBytes)
+		if thisScore < minScore {
+			key = thisKey
+			decrypted = thisDecrypted
+			minScore = thisScore
+			input = line
+		}
+	}
+	fmt.Printf("Challenge 4: input:%s decrypted:%s key:%02X score:%3.3f\n", input, decrypted, key, minScore)
+	if input != "7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f" && key != 0x34 {
+		panic("problem w challenge 4")
+	}
 }
 
 func Challenge5() {
-  data := `Burning 'em, if you ain't quick and nimble
+	data := `Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal`
-  key := "ICE"
+	key := "ICE"
 
-  keyLen := len(key)
+	keyLen := len(key)
 
-  output := make([]byte, len(data))
-  for i, b := range data {
-    output[i] = byte(b) ^ byte(key[i % keyLen])
-  }
-  fmt.Printf("Challenge 5: %x\n", output)
-  if hex.EncodeToString(output) != "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f" {
-    panic("problem w challenge 5")
-  }
+	output := make([]byte, len(data))
+	for i, b := range data {
+		output[i] = byte(b) ^ byte(key[i%keyLen])
+	}
+	fmt.Printf("Challenge 5: %x\n", output)
+	if hex.EncodeToString(output) != "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f" {
+		panic("problem w challenge 5")
+	}
 }
 
 func HammingDistance(b1 []byte, b2 []byte) int {
@@ -272,14 +272,14 @@ func HammingDistance(b1 []byte, b2 []byte) int {
 		xor[i] = b1[i] ^ b2[i]
 	}
 
-  var output int
-  for _, b := range xor {
-    for j := 0; j < 8; j++ {
-      output += int((b >> j) & 0x01)
-    }
+	var output int
+	for _, b := range xor {
+		for j := 0; j < 8; j++ {
+			output += int((b >> j) & 0x01)
+		}
 
-  }
-  return output
+	}
+	return output
 }
 
 // this seems like overkill but i needed to do this to get the right
@@ -288,204 +288,204 @@ func HammingDistance(b1 []byte, b2 []byte) int {
 // this makes the average for all the wrong ones to be tightly
 // distributed around ~3.21 +/0 ~0.005, vs. the correct one is ~2.7
 func CalcDistanceForKeySize(input []byte, keysize int) float64 {
-  chunks := make([][]byte, 0)
-  for i := 0; i < len(input)/keysize; i++ {
-    chunks = append(chunks, input[keysize*i:keysize*(i+1)])
-  }
+	chunks := make([][]byte, 0)
+	for i := 0; i < len(input)/keysize; i++ {
+		chunks = append(chunks, input[keysize*i:keysize*(i+1)])
+	}
 
-  var numerator float64
-  var denominator float64
-  for i, b1 := range chunks {
-    for j, b2 := range chunks {
-      if i != j {
-        numerator += float64(HammingDistance(b1, b2))
-        denominator += 1
-      }
-    }
-  }
-  return numerator / (denominator * float64(keysize))
+	var numerator float64
+	var denominator float64
+	for i, b1 := range chunks {
+		for j, b2 := range chunks {
+			if i != j {
+				numerator += float64(HammingDistance(b1, b2))
+				denominator += 1
+			}
+		}
+	}
+	return numerator / (denominator * float64(keysize))
 }
 
 func TransposeByteArrayByKeySize(byteArray []byte, keysize int) [][]byte {
-  // Break `byteArray` into `keysize` pieces, each of length `byteArray/keysize`
-  // Except some pieces could be 1 entry longer, if byteArray is not equally
-  // divisible by keysize
+	// Break `byteArray` into `keysize` pieces, each of length `byteArray/keysize`
+	// Except some pieces could be 1 entry longer, if byteArray is not equally
+	// divisible by keysize
 
-  output := make([][]byte, keysize)
-  for i := 0; i < keysize; i++ {
-    output[i] = make([]byte, 0)
-  }
+	output := make([][]byte, keysize)
+	for i := 0; i < keysize; i++ {
+		output[i] = make([]byte, 0)
+	}
 
-  iterate := true
-  // stride through `keysize`-length chunks, i is the chunk number
-  // j is the byte number within each chunk, which represents which
-  // output piece it should be put into
-  var i int
-  for iterate {
-    var chunk []byte
-    // if our next stride of length `keysize` goes beyond the end of the array
-    // then just grab whatever's remaining, and this is last iteration of loop
-    if (i+1)*keysize > len(byteArray) {
-      iterate = false
-      chunk = byteArray[i*keysize:]
-    } else {
-      chunk = byteArray[i*keysize:(i+1)*keysize]
-    }
-    for j, b := range chunk {
-      output[j] = append(output[j], b)
-    }
-    i += 1
-  }
-  return output
+	iterate := true
+	// stride through `keysize`-length chunks, i is the chunk number
+	// j is the byte number within each chunk, which represents which
+	// output piece it should be put into
+	var i int
+	for iterate {
+		var chunk []byte
+		// if our next stride of length `keysize` goes beyond the end of the array
+		// then just grab whatever's remaining, and this is last iteration of loop
+		if (i+1)*keysize > len(byteArray) {
+			iterate = false
+			chunk = byteArray[i*keysize:]
+		} else {
+			chunk = byteArray[i*keysize : (i+1)*keysize]
+		}
+		for j, b := range chunk {
+			output[j] = append(output[j], b)
+		}
+		i += 1
+	}
+	return output
 }
 
 func Challenge6() {
-  fmt.Printf("Challenge 6:\n")
-  s1 := "this is a test"
-  s2 := "wokka wokka!!!"
-  if HammingDistance([]byte(s1), []byte(s2)) != 37 {
-    panic("hamming distance wokka wokka test fails")
-  }
+	fmt.Printf("Challenge 6:\n")
+	s1 := "this is a test"
+	s2 := "wokka wokka!!!"
+	if HammingDistance([]byte(s1), []byte(s2)) != 37 {
+		panic("hamming distance wokka wokka test fails")
+	}
 
-  file, err := os.ReadFile("6.txt")
-  if err != nil {
-    panic("problem opening file")
-  }
+	file, err := os.ReadFile("6.txt")
+	if err != nil {
+		panic("problem opening file")
+	}
 
-  input, err := base64.StdEncoding.DecodeString(string(file))
-  if err != nil {
-    panic("problem decoding b64")
-  }
+	input, err := base64.StdEncoding.DecodeString(string(file))
+	if err != nil {
+		panic("problem decoding b64")
+	}
 
-  minDist := 10.0
-  distSlice := make([]float64, 0)
-  var actualKeySize int
-  var idxForKey int
-  for keysize := 2; keysize <= 40; keysize++ {
-    dist := CalcDistanceForKeySize(input, keysize)
-    if dist < minDist {
-      minDist = dist
-      actualKeySize = keysize
-      idxForKey = keysize - 2
-    }
-    distSlice = append(distSlice, dist)
-  }
+	minDist := 10.0
+	distSlice := make([]float64, 0)
+	var actualKeySize int
+	var idxForKey int
+	for keysize := 2; keysize <= 40; keysize++ {
+		dist := CalcDistanceForKeySize(input, keysize)
+		if dist < minDist {
+			minDist = dist
+			actualKeySize = keysize
+			idxForKey = keysize - 2
+		}
+		distSlice = append(distSlice, dist)
+	}
 
-  // remove minimum
-  distSlice = append(distSlice[:idxForKey], distSlice[idxForKey+1:]...)
+	// remove minimum
+	distSlice = append(distSlice[:idxForKey], distSlice[idxForKey+1:]...)
 
-  var distSum float64
-  for _, v := range distSlice {
-    distSum += v
-  }
-  avgDist := distSum / float64(len(distSlice))
+	var distSum float64
+	for _, v := range distSlice {
+		distSum += v
+	}
+	avgDist := distSum / float64(len(distSlice))
 
-  variance := 0.0
-  for _, v := range distSlice {
-    variance += math.Pow(v - avgDist, 2)
-  }
+	variance := 0.0
+	for _, v := range distSlice {
+		variance += math.Pow(v-avgDist, 2)
+	}
 
-  fmt.Printf("\nKeysize: %d MinDist: %3.3f vs others have %3.3f +/- %3.3f\n", actualKeySize, minDist, avgDist, math.Sqrt(variance))
+	fmt.Printf("\nKeysize: %d MinDist: %3.3f vs others have %3.3f +/- %3.3f\n", actualKeySize, minDist, avgDist, math.Sqrt(variance))
 
-  // here i use chunk to signify the transposed result, but inside this
-  // function i use chunk to signify a stride of the original. sorry for
-  // the confused naming
-  transposedChunks := TransposeByteArrayByKeySize(input, actualKeySize)
-  decryptedChunks := make([][]byte, actualKeySize)
-  for i, tc := range transposedChunks {
-    thisDecrypted, _, _ := DecryptSingleByteXOR(tc)
-    decryptedChunks[i] = []byte(thisDecrypted)
-  }
+	// here i use chunk to signify the transposed result, but inside this
+	// function i use chunk to signify a stride of the original. sorry for
+	// the confused naming
+	transposedChunks := TransposeByteArrayByKeySize(input, actualKeySize)
+	decryptedChunks := make([][]byte, actualKeySize)
+	for i, tc := range transposedChunks {
+		thisDecrypted, _, _ := DecryptSingleByteXOR(tc)
+		decryptedChunks[i] = []byte(thisDecrypted)
+	}
 
-  output := make([]byte, 0)
-  // if some chunks have 1 extra entry, the first chunk will
-  for i := 0; i < len(decryptedChunks[0]); i++ {
-    for j := range decryptedChunks {
-      // so make sure to escape once we've used up all extra entries
-      if i >= len(decryptedChunks[j]) {
-        break
-      }
-      output = append(output, decryptedChunks[j][i])
-    }
-  }
+	output := make([]byte, 0)
+	// if some chunks have 1 extra entry, the first chunk will
+	for i := 0; i < len(decryptedChunks[0]); i++ {
+		for j := range decryptedChunks {
+			// so make sure to escape once we've used up all extra entries
+			if i >= len(decryptedChunks[j]) {
+				break
+			}
+			output = append(output, decryptedChunks[j][i])
+		}
+	}
 
-  fmt.Printf("\nSolution:\n%s\n", string(output))
+	fmt.Printf("\nSolution:\n%s\n", string(output))
 
-  if len(input) != len(output) {
-    panic("uh oh, challenge 6 input + output have different lengths")
-  }
+	if len(input) != len(output) {
+		panic("uh oh, challenge 6 input + output have different lengths")
+	}
 }
 
 func Challenge7() {
-  key := []byte("YELLOW SUBMARINE")
-  file, err := os.ReadFile("7.txt")
-  if err != nil {
-    panic("problem opening file")
-  }
-  input, err := base64.StdEncoding.DecodeString(string(file))
+	key := []byte("YELLOW SUBMARINE")
+	file, err := os.ReadFile("7.txt")
+	if err != nil {
+		panic("problem opening file")
+	}
+	input, err := base64.StdEncoding.DecodeString(string(file))
 
-  fmt.Printf("\n ********** Challenge 7 ************\n")
-  block, err := aes.NewCipher(key)
-  if err != nil {
-    fmt.Printf("problem creating block\n")
-  }
-  out := make([]byte, block.BlockSize())
+	fmt.Printf("\n ********** Challenge 7 ************\n")
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Printf("problem creating block\n")
+	}
+	out := make([]byte, block.BlockSize())
 
-  for i := 0; i < len(input); i += 16 {
-    input := input[i:i+16]
-    block.Decrypt(out, input)
-    fmt.Printf("%s", out)
-  }
-  fmt.Printf("********** END Challenge 7 ***********\n")
+	for i := 0; i < len(input); i += 16 {
+		input := input[i : i+16]
+		block.Decrypt(out, input)
+		fmt.Printf("%s", out)
+	}
+	fmt.Printf("********** END Challenge 7 ***********\n")
 }
 
 func Challenge8() {
-  fmt.Printf("********* Challenge 8 ***********\n")
-  file, err := os.ReadFile("8.txt")
-  if err != nil {
-    panic("problem opening file")
-  }
+	fmt.Printf("********* Challenge 8 ***********\n")
+	file, err := os.ReadFile("8.txt")
+	if err != nil {
+		panic("problem opening file")
+	}
 
-  lines := strings.Split(strings.Trim(string(file),"\n"), "\n")
-  for i, line := range lines {
-    input, err := hex.DecodeString(line)
-    if err != nil {
-      panic("can't decode line to hex")
-    }
+	lines := strings.Split(strings.Trim(string(file), "\n"), "\n")
+	for i, line := range lines {
+		input, err := hex.DecodeString(line)
+		if err != nil {
+			panic("can't decode line to hex")
+		}
 
-    nSame := 0
-    segment := make(map[string]int)
-    for j := 0; j < len(input); j += 16 {
-      inputString := hex.EncodeToString(input[j:j+16])
-      if _, ok := segment[inputString]; ok {
-        segment[inputString] += 1
-      } else {
-        segment[inputString] = 1
-      }
+		nSame := 0
+		segment := make(map[string]int)
+		for j := 0; j < len(input); j += 16 {
+			inputString := hex.EncodeToString(input[j : j+16])
+			if _, ok := segment[inputString]; ok {
+				segment[inputString] += 1
+			} else {
+				segment[inputString] = 1
+			}
 
-      for _, v := range segment {
-        if v > nSame {
-          nSame = v
-        }
-      }
-    }
-    if nSame > 1 {
-      fmt.Printf("Line %d has a segment repeated %d times out of %d total segments:\n", i, nSame, len(input) / 16)
-      for j := 0; j < len(input); j += 16 {
-        fmt.Printf("%s\n", hex.EncodeToString(input[j:j+16]))
-      }
-    }
-  }
-  fmt.Printf("******** END Challenge 8 ***********\n")
+			for _, v := range segment {
+				if v > nSame {
+					nSame = v
+				}
+			}
+		}
+		if nSame > 1 {
+			fmt.Printf("Line %d has a segment repeated %d times out of %d total segments:\n", i, nSame, len(input)/16)
+			for j := 0; j < len(input); j += 16 {
+				fmt.Printf("%s\n", hex.EncodeToString(input[j:j+16]))
+			}
+		}
+	}
+	fmt.Printf("******** END Challenge 8 ***********\n")
 }
 
 func main() {
 	Challenge1()
 	Challenge2()
 	Challenge3()
-  Challenge4()
-  Challenge5()
-  Challenge6()
-  Challenge7()
-  Challenge8()
+	Challenge4()
+	Challenge5()
+	Challenge6()
+	Challenge7()
+	Challenge8()
 }
--- set2/main.go
+++ set2/main.go
@@ -11,117 +11,117 @@ import (
 )
 
 func PKCS7(input []byte, blocksize int) []byte {
-  padLength := (blocksize - (len(input) % blocksize))
-  if padLength == blocksize {
-    return input
-  }
+	padLength := (blocksize - (len(input) % blocksize))
+	if padLength == blocksize {
+		return input
+	}
 
-  padding := make([]byte, padLength)
-  for i := range padding {
-    padding[i] = byte(padLength)
-  }
-  return append(input, padding...)
+	padding := make([]byte, padLength)
+	for i := range padding {
+		padding[i] = byte(padLength)
+	}
+	return append(input, padding...)
 }
 
 func PKCS7Unpad(input []byte, blocksize int) []byte {
-  paddingLength := input[len(input)-1]
-  for i := range input {
-    if (i >= (len(input) - int(paddingLength))) && input[i] != paddingLength {
-      return input
-    }
-  }
-  return input[:len(input)-int(paddingLength)]
+	paddingLength := input[len(input)-1]
+	for i := range input {
+		if (i >= (len(input) - int(paddingLength))) && input[i] != paddingLength {
+			return input
+		}
+	}
+	return input[:len(input)-int(paddingLength)]
 }
 
 func Challenge9() {
-  input := []byte("YELLOW SUBMARINE")
-  length := 81
-  padded := PKCS7(input, length)
-  fmt.Printf("Challenge 9: %s\n", padded)
-  if string(padded) != "YELLOW SUBMARINEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" {
-    panic("problem w challenge 9")
-  }
+	input := []byte("YELLOW SUBMARINE")
+	length := 81
+	padded := PKCS7(input, length)
+	fmt.Printf("Challenge 9: %s\n", padded)
+	if string(padded) != "YELLOW SUBMARINEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" {
+		panic("problem w challenge 9")
+	}
 
-  pad := PKCS7(input, 16)
-  if !reflect.DeepEqual(input, pad) {
-    panic("pkcs7 w/no pad required doesn't work")
-  }
+	pad := PKCS7(input, 16)
+	if !reflect.DeepEqual(input, pad) {
+		panic("pkcs7 w/no pad required doesn't work")
+	}
 }
 
 func CBCEncrypt(payload []byte, key []byte, iv []byte) []byte {
-  block, err := aes.NewCipher(key)
-  blockSize := block.BlockSize()
-  if err != nil {
-    fmt.Printf("problem creating block\n")
-  }
+	block, err := aes.NewCipher(key)
+	blockSize := block.BlockSize()
+	if err != nil {
+		fmt.Printf("problem creating block\n")
+	}
 
-  paddedPayload := PKCS7(payload, blockSize)
+	paddedPayload := PKCS7(payload, blockSize)
 
-  out := make([]byte, len(paddedPayload))
+	out := make([]byte, len(paddedPayload))
 
-  for i := 0; i < len(paddedPayload); i += blockSize {
-    var input []byte
-    if i == 0 {
-      input = cryptopals.FixedXOR(iv, paddedPayload[i:i+blockSize])
-    } else {
-      input = cryptopals.FixedXOR(out[i-blockSize:i], paddedPayload[i:i+blockSize])
-    }
-    block.Encrypt(out[i:i+blockSize], input)
-  }
-  return out
+	for i := 0; i < len(paddedPayload); i += blockSize {
+		var input []byte
+		if i == 0 {
+			input = cryptopals.FixedXOR(iv, paddedPayload[i:i+blockSize])
+		} else {
+			input = cryptopals.FixedXOR(out[i-blockSize:i], paddedPayload[i:i+blockSize])
+		}
+		block.Encrypt(out[i:i+blockSize], input)
+	}
+	return out
 }
 
 func CBCDecrypt(payload []byte, key []byte, iv []byte) []byte {
-  block, err := aes.NewCipher(key)
-  blockSize := block.BlockSize()
-  if err != nil {
-    fmt.Printf("problem creating block\n")
-  }
+	block, err := aes.NewCipher(key)
+	blockSize := block.BlockSize()
+	if err != nil {
+		fmt.Printf("problem creating block\n")
+	}
 
-  out := make([]byte, len(payload))
-  rawDecryptedBlock := make([]byte, len(key))
-  decryptedBlock := make([]byte, len(key))
+	out := make([]byte, len(payload))
+	rawDecryptedBlock := make([]byte, len(key))
+	decryptedBlock := make([]byte, len(key))
 
-  for i := 0; i < len(payload); i += blockSize {
-    block.Decrypt(rawDecryptedBlock, payload[i:i+blockSize])
-    if i == 0 {
-      decryptedBlock = cryptopals.FixedXOR(iv, rawDecryptedBlock)
-    } else {
-      decryptedBlock = cryptopals.FixedXOR(payload[i-blockSize:i], rawDecryptedBlock)
-    }
-    copy(out[i:i+blockSize], decryptedBlock)
-  }
-  return PKCS7Unpad(out, blockSize)
+	for i := 0; i < len(payload); i += blockSize {
+		block.Decrypt(rawDecryptedBlock, payload[i:i+blockSize])
+		if i == 0 {
+			decryptedBlock = cryptopals.FixedXOR(iv, rawDecryptedBlock)
+		} else {
+			decryptedBlock = cryptopals.FixedXOR(payload[i-blockSize:i], rawDecryptedBlock)
+		}
+		copy(out[i:i+blockSize], decryptedBlock)
+	}
+	return PKCS7Unpad(out, blockSize)
 }
 
 func Challenge10() {
-  key := []byte("YELLOW SUBMARINE")
-  iv := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-  file, err := os.ReadFile("10.txt")
-  if err != nil {
-    panic("problem opening file")
-  }
-  input, err := base64.StdEncoding.DecodeString(string(file))
+	key := []byte("YELLOW SUBMARINE")
+	iv := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	file, err := os.ReadFile("10.txt")
+	if err != nil {
+		panic("problem opening file")
+	}
+	input, err := base64.StdEncoding.DecodeString(string(file))
 
-  fmt.Printf("\n ********** Challenge 10 ************\n")
+	fmt.Printf("\n ********** Challenge 10 ************\n")
 
-  test := []byte("this is a string and another one what ya now")
-  encrypted := CBCEncrypt(test, key, iv)
-  decrypted := CBCDecrypt(encrypted, key, iv)
-  if !reflect.DeepEqual(test, decrypted) {
-    fmt.Printf("%s %v\n", test, test)
-    fmt.Printf("%v\n", encrypted)
-    fmt.Printf("%s %v\n", decrypted, decrypted)
-    panic("CBC mode roundtrip doesn't work")
-  }
+	test := []byte("this is a string and another one what ya now")
+	encrypted := CBCEncrypt(test, key, iv)
+	decrypted := CBCDecrypt(encrypted, key, iv)
+	if !reflect.DeepEqual(test, decrypted) {
+		fmt.Printf("%s %v\n", test, test)
+		fmt.Printf("%v\n", encrypted)
+		fmt.Printf("%s %v\n", decrypted, decrypted)
+		panic("CBC mode roundtrip doesn't work")
+	}
 
-  decrypted = CBCDecrypt(input, key, iv)
-  fmt.Printf("%s\n", decrypted)
+	decrypted = CBCDecrypt(input, key, iv)
+	fmt.Printf("%s\n", decrypted)
 
-  fmt.Printf("********** END Challenge 10 ***********\n")
+	fmt.Printf("********** END Challenge 10 ***********\n")
 }
 
 func main() {
-  Challenge9()
-  Challenge10()
+	Challenge9()
+	Challenge10()
 }
